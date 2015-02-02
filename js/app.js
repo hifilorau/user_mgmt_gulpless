97254edd6bb7ff981252355e2a1c45e99bcf5cb1
hifilorau <tomrau@gmail.com>
2015-02-01 23:23:17 -0500

    close but no cigars

--- js/app.js
+++ js/app.js
@@ -5,14 +5,13 @@ var emailInput = document.querySelector(".email-input");
 var formInput = document.querySelector(".user-form");
 var userUl= document.querySelector(".users");
 var userContainer = document.querySelector(".user-container");
+var userLi = document.querySelector(".user-li");
 
 var removeButton = document.querySelector(".remove-button");
 
-var myLi = document.querySelector(".user-class");
+// var myLi = document.querySelector(".user-class");
 var userStore = ObjectStore();
 var userArray =  userStore.query();
-
-
 
 
 formInput.addEventListener('submit', function(e){
@@ -21,10 +20,21 @@ formInput.addEventListener('submit', function(e){
   addNewUser();
 });
 
-removeButton.addEventListener('click', function(){
-   removeUser();
+userUl.addEventListener('click', function(e){
+    // If it was a list item
+    if (e.target && e.target.nodeName == "BUTTON"){
+      removeUser();
+    }
 });
 
+// document.getElementById("parent-list").addEventListener("click", function(e) {
+//   // e.target is the clicked element!
+//   // If it was a list item
+//   if(e.target && e.target.nodeName == "LI") {
+//     // List item found!  Output the ID!
+//     console.log("List item ", e.target.id.replace("post-"), " was clicked!");
+//   }
+// });
 
 
 
@@ -33,26 +43,56 @@ function addNewUser() {
   var newUser = {
     firstName: firstNameInput.value,
     lastName: lastNameInput.value,
-    email: emailInput.value
+    email: emailInput.value,
   };
 
   userStore.add(User(newUser));
-  firstNameInput.value = "";
-  lastNameInput.value = "";
-  emailInput.value = "";
+
+  // var myLi = document.createElement('li');
+  // myLi.className = myLi.className + "user-li";
+  // userUl.appendChild(myLi);
+
+//   var liHTML = "";
+//
+//   for (var i = 0; i < userArray.length; ++i ) {
+//  liHTML += "<li class='user-li slideLeft'>" + '<span class="user-li-fn">' + userArray[i].firstName + '</span>' + " " + '<span class="user-li-ln">' + userArray[i].lastName + '</span>' + " " + '<span class="user-li-email">' + userArray[i].email + '</span>' + "<button class='remove-button'>X</button>" + " " + "</li>";
+//   }
+//
+//   liHTML.concat(liHTML);
+//   userUl.innerHTML = liHTML;
+//   // console.log(userStore.query());
 
   var liHTML = "";
 
   for (var i = 0; i < userArray.length; ++i ) {
- liHTML += "<li class='user-class'>" + userArray[i].firstName + " " + userArray[i].lastName + " " + userArray[i].email + "<button class='remove-button'>x</button>" + "</li>";
+ liHTML = '<span class="user-li-fn">' + userArray[i].firstName + '</span>' + " " + '<span class="user-li-ln">' + userArray[i].lastName + '</span>' + " " + '<span class="user-li-email">' + userArray[i].email + '</span>' + "<button class='remove-button'>X</button>";
   }
-
+  var myLi = document.createElement('li');
+  myLi.className = myLi.className + "user-li slideLeft";
+  userUl.appendChild(myLi);
   liHTML.concat(liHTML);
-  userUl.innerHTML = liHTML;
+  myLi.innerHTML = liHTML;
+
+  // sets values back to empty in the inputs
+  firstNameInput.value = "";
+  lastNameInput.value = "";
+  emailInput.value = "";
   // console.log(userStore.query());
+
 };
 
 
+
 function removeUser() {
-  userStore.remove(newUser);
+    // var userLi = document.querySelectorAll(".user-li");
+    var userLi = document.querySelector(".user-li");
+    // for (var i=0; i<userLi.length; ++i){
+    userLi.parentNode.removeChild(userLi);
+
+    // userStore.remove(userArray[i]);
+
+//   var userLi = this.parentNode;
+// //Remove the parent list item from the ul
+//   userUl.removeChild(userLi);
+//   var elem = document.getElementById('id');
 }
--- js/user.js
+++ js/user.js
@@ -1,5 +1,6 @@
 // User constructs a new user object. For more details, see
 // https://github.com/tiy-durham-fe-2015/curriculum/tree/master/assignments/user_mgmt
+
 function User(spec) {
   if (!spec.firstName || spec.firstName.trim() === ''){
     throw 'First name is required';
@@ -9,6 +10,7 @@ var self = {
     firstName: spec.firstName,
     lastName: spec.lastName,
     email: spec.email,
+    userId: spec.userId,
 
     equal: function(otherUser) {
       return self.email === otherUser.email;
